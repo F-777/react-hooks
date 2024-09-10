9917862fed113b87cb17249d415e54f294121ef1
Admojo Revo 529 <imanuelrevoadmojo@gmail.com>
2024-09-10 09:37:39 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Card from "./Components/Card";
 import Main from  "./Components/Main";
 import "./Components/style.css";
 
 function App() {
   return (
     <>
+      <Card />
       <Main />
     </>
   )
